9c89a8cb3ca5c7e84c46d4629deb2817d2b7a201
nieyuzhou <rucnyz@gmail.com>
2021-11-14 19:51:59 +0800

    数据库连接

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
-
+from json import load
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
-# 还需要安装pymysql库
+
 # 初始化app
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
+app.config.from_file("./mssql_config.json",load=load)
 # 连接数据库
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:nyz010622@localhost/test'
-
-
-
+db = SQLAlchemy(app)
 
 # SQLALCHEMY_DATABASE_URI
 
@@ -22,13 +20,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:nyz010622@localhos
 # Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。
 # 这需要额外的内存， 如果不必要的可以禁用它。
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
 # 得到SQLAlchemy对象
 
-db = SQLAlchemy(app, use_native_unicode='utf8')
 
 @app.route("/")
-def hello():
-    cursor = db.engine.execute("select * from dept")
+def run():
+    # 是这一句出问题了，似乎不能在执行的函数内初始化数据库的连接，
+    # 而需要放在外部，注释掉就正常了
+    # db = SQLAlchemy(app, use_native_unicode = 'utf8')
+    cursor = db.engine.execute("select * from master.dbo.sysdatabases")
     ret = cursor.fetchone()
     return str(ret)
 
+
+app.run(debug = True)
