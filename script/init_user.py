b77c1cbb840bddc8d93aff94ba4d5b5d194b6564
corporal <corporal9736@gmail.com>
2021-12-09 00:54:58 +0800

    添加数据库用户名，一些小补丁

--- script/init_user.py
+++ script/init_user.py
@@ -2,23 +2,44 @@ import sys
 sys.path.append(".")
 from utils import run_sql
 
-create_customer = """
+create_login_customer = """
     CREATE LOGIN customer WITH PASSWORD = 'PasswordForCustomer',
     CHECK_POLICY = OFF;
 """
 
-run_sql(create_customer)
+run_sql(create_login_customer)
 
-create_supplier = """
+create_login_supplier = """
     CREATE LOGIN supplier WITH PASSWORD = 'PasswordForSupplier',
     CHECK_POLICY = OFF;
 """
 
-run_sql(create_supplier)
+run_sql(create_login_supplier)
 
-create_admin = """
+create_login_admin = """
     CREATE LOGIN admin WITH PASSWORD = 'PasswordForAdmin',
     CHECK_POLICY = OFF;
 """
 
-run_sql(create_admin)
+run_sql(create_login_admin)
+
+create_user_customer = """
+    USE OnlineShopping;
+    CREATE USER customer FROM LOGIN customer
+"""
+
+run_sql(create_user_customer)
+
+create_user_supplier = """
+    USE OnlineShopping;
+    CREATE USER supplier FROM LOGIN supplier
+"""
+
+run_sql(create_user_supplier)
+
+create_user_admin = """
+    USE OnlineShopping;
+    CREATE USER admin FROM LOGIN admin
+"""
+
+run_sql(create_user_admin)
