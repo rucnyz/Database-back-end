5bf4089ab5b584dbc470f46e3e6571d0522ed639
zzm <zimmyx@163.com>
2021-12-20 12:33:42 +0800

    修改app.py的bug

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.register_blueprint(homepage, url_prefix='/api/HomePage')
 app.register_blueprint(test, url_prefix='/api/Test')
 app.register_blueprint(customer, url_prefix='/api/customer')
 app.register_blueprint(comment, url_prefix='/api/comment')
-app.register_blueprint(supplier, url_preflix='/api/supplier')
+app.register_blueprint(supplier, url_prefix='/api/supplier')
 app.register_blueprint(admin, url_prefix='/api/admin')
 app.register_blueprint(product, url_prefix='/api/product')
 
