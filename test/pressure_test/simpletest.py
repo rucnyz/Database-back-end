a95ee56d9821df679afc3c0ca9c58cb3fc016048
nieyuzhou <rucnyz@gmail.com>
2021-12-22 09:10:56 +0800

    压测任务中homepage简单修改

--- test/pressure_test/simpletest.py
+++ test/pressure_test/simpletest.py
@@ -6,12 +6,12 @@ class QuickstartUser(FastHttpUser):
 
     @task
     def home_page(self):
-        self.client.get("/api/HomePage/getProduct?needNumber=20&page=1")
+        self.client.get("/api/HomePage/getProduct?needNumber=20&page=%s" % random.randint(1, 5))
         self.client.get("/api/HomePage/getCategory")
 
     @task
     def customer_info(self):
-        self.client.post(f"/api/customer/C{'{:0>9d}'.format(random.randint(1,50))}/info")
+        self.client.post(f"/api/customer/C{'{:0>9d}'.format(random.randint(1, 50))}/info")
 
     # @task
     def hello_world(self):
