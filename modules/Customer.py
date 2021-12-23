038116650570fbbd499471301cc2e595eeddea89
nieyuzhou <rucnyz@gmail.com>
2021-12-23 23:30:58 +0800

    订单列表返回productID用于点击图片

--- modules/Customer.py
+++ modules/Customer.py
@@ -167,7 +167,7 @@ def update_customer_info(customerID):
     phone_number = request.json['phoneNumber']
     update_customer_info = """
     UPDATE info_customer
-    SET nickname=:nickname, phone=:phone
+    SET nickname=:nickname, phone=:phone, address_name=:address_name
     WHERE customer_id=:customer_id AND address_name=:address_name
     """
     _ = run_sql(update_customer_info, {"address_name": address,
@@ -355,7 +355,7 @@ def orders_add_cart(id):  # 新订单添加
             from orders  
              """
             tuple_tmp = run_sql(getNum)
-            order_id_new = 'O' + str(int(tuple_tmp[0]['cnt'] + 1)).zfill(9)  # 获得新的订单编号
+            order_id_new = 'O' + str(int(tuple_tmp[0]['cnt'] + 101)).zfill(9)  # 获得新的订单编号
 
             orders_add = """
             INSERT
@@ -427,7 +427,7 @@ def orders_add_product(id):  # 新订单添加
         """
 
         tuple_tmp = run_sql(getNum)
-        order_id_new = 'O' + str(int(tuple_tmp[0]['cnt'] + 1)).zfill(9)  # 获得新的订单编号
+        order_id_new = 'O' + str(int(tuple_tmp[0]['cnt'] + 101)).zfill(9)  # 获得新的订单编号
 
         getInfo = """
         SELECT info_s.address_name da, s.supplier_id sid
